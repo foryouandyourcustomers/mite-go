81402d7120ef7454f81187a74113560e66925334
Ulrich Lissé <ulrich.lisse@leanovate.de>
2019-04-02 17:07:10 +0200

    Extract service entity

--- mite/api.go
+++ mite/api.go
@@ -17,12 +17,6 @@ type MiteApi interface {
 	TimeEntries(params *TimeEntryParameters) ([]TimeEntry, error)
 }
 
-type Service struct {
-	Id   string
-	Name string
-	Note string
-}
-
 type TimeEntry struct {
 	Id          string
 	Note        string
@@ -53,50 +47,6 @@ type defaultApi struct {
 
 func NewMiteApi(url string, key string) MiteApi {
 	return &defaultApi{url: url, key: key, client: &http.Client{}}
-}
-
-func (a *defaultApi) Services() ([]Service, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/%s", a.url, "services.json"), nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("X-MiteApiKey", a.key)
-	req.Header.Add("User-Agent", userAgent)
-
-	res, err := a.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer func() { _ = res.Body.Close() }()
-
-	srs := []ServiceResponse{}
-	err = json.NewDecoder(res.Body).Decode(&srs)
-	if err != nil {
-		return nil, err
-	}
-
-	services := []Service{}
-	for _, sr := range srs {
-		services = append(services, sr.ToService())
-	}
-
-	return services, nil
-}
-
-type ServiceResponse struct {
-	Service struct {
-		Id   int    `json:"id"`
-		Name string `json:"name"`
-		Note string `json:"note"`
-	} `json:"service"`
-}
-
-func (r ServiceResponse) ToService() Service {
-	return Service{
-		Id:   fmt.Sprintf("%d", r.Service.Id),
-		Name: r.Service.Name,
-		Note: r.Service.Note,
-	}
 }
 
 func (a *defaultApi) TimeEntries(params *TimeEntryParameters) ([]TimeEntry, error) {
