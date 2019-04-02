d1fc299087256e12a3581512a9e330268e12242c
Ulrich Lissé <ulrich.lisse@leanovate.de>
2019-04-02 17:05:25 +0200

    Extract project entity

--- mite/api.go
+++ mite/api.go
@@ -17,12 +17,6 @@ type MiteApi interface {
 	TimeEntries(params *TimeEntryParameters) ([]TimeEntry, error)
 }
 
-type Project struct {
-	Id   string
-	Name string
-	Note string
-}
-
 type Service struct {
 	Id   string
 	Name string
@@ -59,50 +53,6 @@ type defaultApi struct {
 
 func NewMiteApi(url string, key string) MiteApi {
 	return &defaultApi{url: url, key: key, client: &http.Client{}}
-}
-
-func (a *defaultApi) Projects() ([]Project, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/%s", a.url, "projects.json"), nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("X-MiteApiKey", a.key)
-	req.Header.Add("User-Agent", userAgent)
-
-	res, err := a.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer func() { _ = res.Body.Close() }()
-
-	prs := []ProjectResponse{}
-	err = json.NewDecoder(res.Body).Decode(&prs)
-	if err != nil {
-		return nil, err
-	}
-
-	projects := []Project{}
-	for _, pr := range prs {
-		projects = append(projects, pr.ToProject())
-	}
-
-	return projects, nil
-}
-
-type ProjectResponse struct {
-	Project struct {
-		Id   int    `json:"id"`
-		Name string `json:"name"`
-		Note string `json:"note"`
-	} `json:"project"`
-}
-
-func (r ProjectResponse) ToProject() Project {
-	return Project{
-		Id:   fmt.Sprintf("%d", r.Project.Id),
-		Name: r.Project.Name,
-		Note: r.Project.Note,
-	}
 }
 
 func (a *defaultApi) Services() ([]Service, error) {
