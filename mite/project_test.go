86ae27080ed0f6d3ce383473118167408fb7e9dd
Ulrich Lissé <ulrich.lisse@leanovate.de>
2019-04-09 17:07:00 +0200

    Refactor project and services tests

--- mite/project_test.go
+++ mite/project_test.go
@@ -11,33 +11,40 @@ import (
 )
 
 const projectResponse = `{
-   "project": {
-      "id": 643,
-      "name": "Open-Source",
-      "note": "valvat, memento et all.",
-      "customer_id": 291,
-      "customer_name": "Yolk",
-      "budget": 0,
-      "budget_type": "minutes",
-      "hourly_rate": 6000,
-      "archived": false,
-      "active_hourly_rate": "hourly_rate",
-      "hourly_rates_per_service": [
-         {
-            "service_id": 31272,
-            "hourly_rate": 4500
-         },
-         {
-            "service_id": 149228,
-            "hourly_rate": 5500
-         }
-      ],
-      "created_at": "2011-08-17T12:06:57+02:00",
-      "updated_at": "2015-02-19T10:53:10+01:00"
-   }
+  "project": {
+    "id": 643,
+    "name": "Open-Source",
+    "note": "valvat, memento et all.",
+    "customer_id": 291,
+    "customer_name": "Yolk",
+    "budget": 0,
+    "budget_type": "minutes",
+    "hourly_rate": 6000,
+    "archived": false,
+    "active_hourly_rate": "hourly_rate",
+    "hourly_rates_per_service": [
+      {
+        "service_id": 31272,
+        "hourly_rate": 4500
+      },
+      {
+        "service_id": 149228,
+        "hourly_rate": 5500
+      }
+    ],
+    "created_at": "2011-08-17T12:06:57+02:00",
+    "updated_at": "2015-02-19T10:53:10+01:00"
+  }
 }`
 
+var projectObject = domain.Project{
+	Id:   domain.NewProjectId(643),
+	Name: "Open-Source",
+	Note: "valvat, memento et all.",
+}
+
 func TestApi_Projects(t *testing.T) {
+	// given
 	rec := recorder{}
 	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		rec.method = r.Method
@@ -47,19 +54,19 @@ func TestApi_Projects(t *testing.T) {
 
 		w.Header().Add("Content-Type", "application/json; charset=utf-8")
 		w.WriteHeader(200)
-		w.Write([]byte(fmt.Sprintf("[%s]", projectResponse)))
+		_, _ = w.Write([]byte(fmt.Sprintf("[%s]", projectResponse)))
 	}))
 
 	defer srv.Close()
 
 	api := mite.NewApi(srv.URL, testApiKey, testClientVersion)
+
+	// when
 	projects, err := api.Projects()
 
+	// then
 	assert.Nil(t, err)
-	assert.Equal(t, []*domain.Project{{
-		Id:   domain.NewProjectId(643),
-		Name: "Open-Source",
-		Note: "valvat, memento et all."}}, projects)
+	assert.Equal(t, []*domain.Project{&projectObject}, projects)
 
 	assert.Equal(t, http.MethodGet, rec.method)
 	assert.Equal(t, "/projects.json", rec.url)
